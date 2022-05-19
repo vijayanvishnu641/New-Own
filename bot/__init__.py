3767edea6df3875c47abafc0c17f2bd9173c9367
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-05-19 10:23:20 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -31,6 +31,16 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 LOGGER = logging.getLogger(__name__)
 
+CONFIG_FILE_URL = os.environ.get('CONFIG_FILE_URL', None)
+if CONFIG_FILE_URL is not None:
+    res = requests.get(CONFIG_FILE_URL)
+    if res.status_code == 200:
+        with open('config.env', 'wb+') as f:
+            f.write(res.content)
+            f.close()
+    else:
+        logging.error(res.status_code)
+
 load_dotenv('config.env')
 
 Interval = []
