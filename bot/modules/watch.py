6509750c07c28812bad4ca45d1aa797a2f253d33
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-05-22 14:19:12 +0530

    Update watch.py

--- bot/modules/watch.py
+++ bot/modules/watch.py
@@ -1,8 +1,7 @@
 from telegram.ext import CommandHandler
 from telegram import Bot, Update
-from bot import Interval, DOWNLOAD_DIR, DOWNLOAD_STATUS_UPDATE_INTERVAL, dispatcher, LOGGER
-from bot.helper.ext_utils.bot_utils import setInterval
-from bot.helper.telegram_helper.message_utils import update_all_messages, sendMessage, sendStatusMessage
+from bot import DOWNLOAD_DIR, dispatcher, LOGGER
+from bot.helper.telegram_helper.message_utils import sendMessage, sendStatusMessage
 from .mirror import MirrorListener
 from bot.helper.mirror_utils.download_utils.youtube_dl_download_helper import YoutubeDLHelper
 from bot.helper.telegram_helper.bot_commands import BotCommands
@@ -10,10 +9,11 @@ from bot.helper.telegram_helper.filters import CustomFilters
 import threading
 
 
-def _watch(bot: Bot, update, isTar=False):
+def _watch(bot: Bot, update, isTar=False, isZip=False, isLeech=False):
     mssg = update.message.text
     message_args = mssg.split(' ')
     name_args = mssg.split('|')
+    
     try:
         link = message_args[1]
     except IndexError:
@@ -24,6 +24,7 @@ def _watch(bot: Bot, update, isTar=False):
         msg += "This file will be downloaded in 720p quality and it's name will be <b>Slam</b>"
         sendMessage(msg, bot, update)
         return
+    
     try:
       if "|" in mssg:
         mssg = mssg.split("|")
@@ -36,35 +37,52 @@ def _watch(bot: Bot, update, isTar=False):
         qual = f'bestvideo[height<={qual}]+bestaudio/best[height<={qual}]'
     except IndexError:
       qual = "bestvideo+bestaudio/best"
+    
     try:
       name = name_args[1]
     except IndexError:
       name = ""
-    reply_to = update.message.reply_to_message
-    if reply_to is not None:
-        tag = reply_to.from_user.username
-    else:
-        tag = None
+    
     pswd = ""
-    listener = MirrorListener(bot, update, pswd, isTar, tag)
+    listener = MirrorListener(bot, update, pswd, isTar, isZip=isZip, isLeech=isLeech)
     ydl = YoutubeDLHelper(listener)
     threading.Thread(target=ydl.add_download,args=(link, f'{DOWNLOAD_DIR}{listener.uid}', qual, name)).start()
     sendStatusMessage(update, bot)
-    if len(Interval) == 0:
-        Interval.append(setInterval(DOWNLOAD_STATUS_UPDATE_INTERVAL, update_all_messages))
-
-
-def watchTar(update, context):
-    _watch(context.bot, update, True)
-
 
 def watch(update, context):
     _watch(context.bot, update)
 
+def watchTar(update, context):
+    _watch(context.bot, update, True)
 
-mirror_handler = CommandHandler(BotCommands.WatchCommand, watch,
+def watchZip(update, context):
+    _watch(context.bot, update, True, True)
+
+def leechWatch(update, context):
+    _watch(context.bot, update, isLeech=True)
+
+def leechWatchTar(update, context):
+    _watch(context.bot, update, True, isLeech=True)
+
+def leechWatchZip(update, context):
+    _watch(context.bot, update, True, True, True)
+
+watch_handler = CommandHandler(BotCommands.WatchCommand, watch,
                                 filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
-tar_mirror_handler = CommandHandler(BotCommands.TarWatchCommand, watchTar,
+tar_watch_handler = CommandHandler(BotCommands.TarWatchCommand, watchTar,
                                     filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
-dispatcher.add_handler(mirror_handler)
-dispatcher.add_handler(tar_mirror_handler)
+zip_watch_handler = CommandHandler(BotCommands.ZipWatchCommand, watchZip,
+                                    filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+leech_watch_handler = CommandHandler(BotCommands.LeechWatchCommand, leechWatch,
+                                filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+leech_tar_watch_handler = CommandHandler(BotCommands.LeechTarWatchCommand, leechWatchTar,
+                                    filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+leech_zip_watch_handler = CommandHandler(BotCommands.LeechZipWatchCommand, leechWatchZip,
+                                    filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+
+dispatcher.add_handler(watch_handler)
+dispatcher.add_handler(tar_watch_handler)
+dispatcher.add_handler(zip_watch_handler)
+dispatcher.add_handler(leech_watch_handler)
+dispatcher.add_handler(leech_tar_watch_handler)
+dispatcher.add_handler(leech_zip_watch_handler)
