707eb6d0af6e4e88c59bb9340dcae9f43347c5dd
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-05-23 01:14:52 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -1,7 +1,3 @@
-# Implement By - @anasty17 (https://github.com/SlamDevs/slam-mirrorbot/commit/d888a1e7237f4633c066f7c2bbfba030b83ad616)
-# (c) https://github.com/SlamDevs/slam-mirrorbot
-# All rights reserved
-
 import os
 import logging
 import time
@@ -16,7 +12,7 @@ from bot.helper.ext_utils.fs_utils import take_ss
 LOGGER = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-VIDEO_SUFFIXES = ("MKV", "MP4", "MOV", "WMV", "3GP", "MPG", "WEBM", "AVI", "FLV", "M4V")
+VIDEO_SUFFIXES = ("M4V", "MP4", "MOV", "FLV", "WMV", "3GP", "MPG", "WEBM", "MKV", "AVI")
 AUDIO_SUFFIXES = ("MP3", "M4A", "M4B", "FLAC", "WAV", "AIF", "OGG", "AAC", "DTS", "MID", "AMR", "MKA")
 IMAGE_SUFFIXES = ("JPG", "JPX", "PNG", "GIF", "WEBP", "CR2", "TIF", "BMP", "JXR", "PSD", "ICO", "HEIC")
 
@@ -48,15 +44,16 @@ class TgUploader:
                 if self.is_cancelled:
                     return
                 up_path = os.path.join(dirpath, file)
-                self.upload_file(up_path, file, dirpath)
+                self.upload_file(up_path, file)
                 if self.is_cancelled:
                     return
                 msgs_dict[file] = self.sent_msg.message_id
+                os.remove(up_path)
                 self.last_uploaded = 0
         LOGGER.info(f"Leech Done: {self.name}")
         self.__listener.onUploadComplete(self.name, None, msgs_dict, None, None)
 
-    def upload_file(self, up_path, file, dirpath):
+    def upload_file(self, up_path, file):
         cap_mono = f"<code>{file}</code>"
         notMedia = False
         thumb = self.thumb
@@ -64,25 +61,21 @@ class TgUploader:
             if not self.as_doc:
                 duration = 0
                 if file.upper().endswith(VIDEO_SUFFIXES):
+                    width = 0
+                    height = 0
                     metadata = extractMetadata(createParser(up_path))
                     if metadata.has("duration"):
                         duration = metadata.get("duration").seconds
                     if thumb is None:
-                        thumb = take_ss(up_path)
+                        thumb, width = take_ss(up_path, duration)
                     if self.is_cancelled:
                         return
-                    if not file.upper().endswith(("MKV", "MP4")):
-                        file = os.path.splitext(file)[0] + '.mp4'
-                        new_path = os.path.join(dirpath, file)
-                        os.rename(up_path, new_path)
-                        up_path = new_path
                     self.sent_msg = self.sent_msg.reply_video(video=up_path,
                                                               quote=True,
                                                               caption=cap_mono,
-                                                              parse_mode="html",
                                                               duration=duration,
-                                                              width=480,
-                                                              height=320,
+                                                              width=width,
+                                                              height=height,
                                                               thumb=thumb,
                                                               supports_streaming=True,
                                                               disable_notification=True,
@@ -98,7 +91,6 @@ class TgUploader:
                     self.sent_msg = self.sent_msg.reply_audio(audio=up_path,
                                                               quote=True,
                                                               caption=cap_mono,
-                                                              parse_mode="html",
                                                               duration=duration,
                                                               performer=artist,
                                                               title=title,
@@ -109,27 +101,18 @@ class TgUploader:
                     self.sent_msg = self.sent_msg.reply_photo(photo=up_path,
                                                               quote=True,
                                                               caption=cap_mono,
-                                                              parse_mode="html",
+                                                              supports_streaming=True,
                                                               disable_notification=True,
                                                               progress=self.upload_progress)
                 else:
                     notMedia = True
             if self.as_doc or notMedia:
-                if file.upper().endswith(VIDEO_SUFFIXES) and thumb is None:
-                    thumb = take_ss(up_path)
-                if self.is_cancelled:
-                    return
                 self.sent_msg = self.sent_msg.reply_document(document=up_path,
                                                              quote=True,
                                                              thumb=thumb,
                                                              caption=cap_mono,
-                                                             parse_mode="html",
                                                              disable_notification=True,
                                                              progress=self.upload_progress)
-                if self.thumb is None and thumb is not None and os.path.lexists(thumb):
-                    os.remove(thumb)
-            if not self.is_cancelled:
-                os.remove(up_path)
         except FloodWait as f:
             LOGGER.info(f)
             time.sleep(f.x)
