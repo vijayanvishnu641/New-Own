1c2ee6df743e7db88e88d67b753676f290a85ee5
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-05-23 16:28:23 +0530

    Update filters.py

--- bot/helper/telegram_helper/filters.py
+++ bot/helper/telegram_helper/filters.py
@@ -1,51 +1,33 @@
 from telegram.ext import MessageFilter
 from telegram import Message
-from bot import AUTHORIZED_CHATS, SUDO_USERS, OWNER_ID, download_dict, download_dict_lock
+from bot import AUTHORIZED_CHATS, SUDO_USERS, OWNER_ID
 
 
 class CustomFilters:
-    class _OwnerFilter(MessageFilter):
-        def filter(self, message):
+    class __OwnerFilter(MessageFilter):
+        def filter(self, message: Message):
             return bool(message.from_user.id == OWNER_ID)
 
-    owner_filter = _OwnerFilter()
+    owner_filter = __OwnerFilter()
 
-    class _AuthorizedUserFilter(MessageFilter):
-        def filter(self, message):
+    class __AuthorizedUserFilter(MessageFilter):
+        def filter(self, message: Message):
             id = message.from_user.id
             return bool(id in AUTHORIZED_CHATS or id in SUDO_USERS or id == OWNER_ID)
 
-    authorized_user = _AuthorizedUserFilter()
+    authorized_user = __AuthorizedUserFilter()
 
-    class _AuthorizedChat(MessageFilter):
-        def filter(self, message):
+    class __AuthorizedChat(MessageFilter):
+        def filter(self, message: Message):
             return bool(message.chat.id in AUTHORIZED_CHATS)
 
-    authorized_chat = _AuthorizedChat()
+    authorized_chat = __AuthorizedChat()
 
-    class _SudoUser(MessageFilter):
-        def filter(self,message):
+    class __SudoUser(MessageFilter):
+        def filter(self, message: Message):
             return bool(message.from_user.id in SUDO_USERS)
 
-    sudo_user = _SudoUser()
+    sudo_user = __SudoUser()
 
-    class _MirrorOwner(MessageFilter):
-        def filter(self, message: Message):
-            user_id = message.from_user.id
-            if user_id == OWNER_ID:
-                return True
-            args = str(message.text).split(' ')
-            if len(args) > 1:
-                # Cancelling by gid
-                with download_dict_lock:
-                    for message_id, status in download_dict.items():
-                        if status.gid() == args[1] and status.message.from_user.id == user_id:
-                            return True
-                    else:
-                        return False
-            if not message.reply_to_message and len(args) == 1:
-                return True
-            # Cancelling by replying to original mirror message
-            reply_user = message.reply_to_message.from_user.id
-            return bool(reply_user == user_id)
-    mirror_owner_filter = _MirrorOwner()
+    def _owner_query(user_id):
+        return bool(user_id == OWNER_ID or user_id in SUDO_USERS)
